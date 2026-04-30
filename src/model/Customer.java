e9f64c2d6bcbc81640f09c5910a75f70388c7db1
poojapatil-debug <poojachapatil@gmail.com>
2026-04-30 13:07:08 +0530

    second commit

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -4,23 +4,23 @@ package model;
 public class Customer {
     private int id;
     private String fullname;
-    private String phone;
+    private String phone11;
 
-    public Customer(int id, String name, String phone) {
+    public Customer(int id, String name, String phone11) {
         this.id = id;
         this.fullname = name;
-        this.phone = phone;
+        this.phone11 = phone11;
     }
 
     public int getId() { return id; }
     public String getName() { return fullname; }
-    public String getPhone() { return phone; }
+    public String getphone11() { return phone11; }
 
     public void setName(String name) { this.fullname = name; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public void setphone11(String phone11) { this.phone11 = phone11; }
 
     @Override
     public String toString() {
-        return id + " | " + name + " | " + phone;
+        return id + " | " + name + " | " + phone11;
     }
 }
